0a11af1b578d1e5a08df24f8df72ac2b46e55765
Pragathi Nataraj <pragathi.nataraj@publicissapient.com>
2020-07-06 13:47:19 +0530

    logger on action dispatched

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ import { createStore, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import thunk from "redux-thunk";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import charactersReducer from "./store/reducers/characters";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("[Middleware] Dispatching action", action);
+      const result = next(action);
+      console.log("next state", store.getState());
+      return result;
+    };
+  };
+};
+
 const store = createStore(
   charactersReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(logger, thunk))
 );
 
 const app = (
